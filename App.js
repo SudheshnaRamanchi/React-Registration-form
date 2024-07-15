4b3a851d2a1a22c7b6a8f7e570829845ed9359cb
SudheshnaRamanchi <160815654+SudheshnaRamanchi@users.noreply.github.com>
2024-07-15 20:55:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './RegistrationForm'; // Corrected import statement
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
